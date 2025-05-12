46b9025ff34e9cca0b1779b5040f18ecbdfacbd8
Sai26reddy <eguturisai26@gmail.com>
2025-05-12 15:52:57 +0530

    executed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-          System.out.println("jenkins practice");
+          System.out.println("jenkins sai reddy implemented");
         }
 
 }
